cc75db78739b8e751eb59fa588a031470dec2475
Huazhong Ji <hzji210@gmail.com>
2025-05-26 11:39:31 +0800

    Update vllm_worker_wrap.py

--- openrlhf/trainer/ray/vllm_worker_wrap.py
+++ openrlhf/trainer/ray/vllm_worker_wrap.py
@@ -1,18 +1,19 @@
-class WorkerWrap:
+from vllm.worker.worker import Worker
+from openrlhf import ACCELERATOR_TYPE
+if ACCELERATOR_TYPE == "NPU":
+    from vllm_ascend.worker.worker import NPUWorker as Worker
+class WorkerWrap(Worker):
     def init_process_group(
         self, master_address, master_port, rank_offset, world_size, group_name, backend="nccl", use_ray=False
     ):
         """Init torch process group for model weights update"""
         import torch
         from openrlhf.utils.distributed_util import init_process_group
-
         assert torch.distributed.is_initialized(), f"default torch process group must be initialized"
         assert group_name != "", f"group name must not be empty"
-
         rank = torch.distributed.get_rank() + rank_offset
         if use_ray:
             import ray.util.collective as collective
-
             collective.init_collective_group(world_size=world_size, rank=rank, backend=backend, group_name=group_name)
             self._model_update_group = group_name
         else:
@@ -28,39 +29,29 @@ class WorkerWrap:
             f"init_process_group: master_address={master_address}, master_port={master_port}, ",
             f"rank={rank}, world_size={world_size}, group_name={group_name}",
         )
-
     def update_weight(self, name, dtype, shape, empty_cache=False):
         import torch
-
         """Broadcast weight to all vllm workers from source rank 0 (actor model)"""
         if torch.distributed.get_rank() == 0:
             print(f"update weight: {name}, dtype: {dtype}, shape: {shape}")
-
         assert dtype == self.model_config.dtype, f"mismatch dtype: src {dtype}, dst {self.model_config.dtype}"
         weight = torch.empty(shape, dtype=dtype, device="cuda")
         if self._model_update_with_ray:
             import ray.util.collective as collective
-
             collective.broadcast(weight, 0, group_name=self._model_update_group)
         else:
             torch.distributed.broadcast(weight, 0, group=self._model_update_group)
-
         self.model_runner.model.load_weights(weights=[(name, weight)])
-
         del weight
         # TODO: should we empty cache if all weights have updated?
         # if empty_cache:
         #     torch.cuda.empty_cache()
-
     def update_weight_cuda_ipc(self, name, dtype, shape, ipc_handles=None, empty_cache=False):
         import torch
         from openrlhf.trainer.ray.utils import get_physical_gpu_id
-
         if torch.distributed.get_rank() == 0:
             print(f"update weight: {name}, dtype: {dtype}, shape: {shape}")
-
         assert dtype == self.model_config.dtype, f"mismatch dtype: src {dtype}, dst {self.model_config.dtype}"
-
         handle = ipc_handles[get_physical_gpu_id()]
         device_id = self.device.index
         func, args = handle
